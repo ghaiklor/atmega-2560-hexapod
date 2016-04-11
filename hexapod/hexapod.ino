2fdf3ca8c2e93cb5d0d87ff57ebd48e9979d335f
Eugene Obrezkov <ghaiklor@gmail.com>
2016-04-11 15:03:20 +0300

    Update timer

--- hexapod/hexapod.ino
+++ hexapod/hexapod.ino
@@ -10,12 +10,19 @@ void walkSpider() {
   spider.walk();
 }
 
+void randomParams() {
+  spider.setSpeed(random(5, 10));
+  spider.setAngle(random(0, 45));
+}
+
 void setup() {
+  randomSeed(analogRead(A0));
   spider.attach(PINS);
-  spider.setSpeed(1);
+  spider.setSpeed(10);
   timer.setInterval(20, walkSpider);
+  timer.setInterval(10000, randomParams);
 }
 
 void loop() {
-//  timer.run();
+  timer.run();
 }
